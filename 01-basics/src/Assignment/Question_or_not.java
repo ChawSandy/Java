fbd86d040a62a460b41300c034503ec651712098
Chaw Sandy <rosyblossom2@gmail.com>
2022-07-14 19:16:46 +0630

    ques_assignment

--- 01-basics/src/Assignment/Question_or_not.java
+++ 01-basics/src/Assignment/Question_or_not.java
@@ -1,44 +1,48 @@
 package Assignment;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Question_or_not {
 
 	public static void main(String[] args) {
 
-		Scanner sc =  new Scanner(System.in);
-		System.out.println("Enter a sentance: ");
-		String sentence = sc.nextLine();
-		
-		String[] data = sentence.split(" ");
+		Scanner sc = new Scanner(System.in);
+			System.out.println("Enter a sentance: ");
+			String sentence = sc.nextLine();
+			String[] ques = new String[] {"Did","did","Were","were","was","Was"};
+			
+
+			String[] data = sentence.split(" ");
 //		System.out.println(Arrays.toString(data));
-		if(sentence.endsWith("?")) {
-			System.out.println("It is question sentence.");
-			String first = data[0];
-			System.out.println("First of the sentence: "+first);
-			
-			String[] ques = new String[] {"Did","did","Where","were","was","Was"};
-			int result = -1;
-			
-			for(int i = 0 ; i < ques.length ;i++) {
-				if(sentence.contains(ques[i])){
-					result = 1;
-					break;
+			if(sentence.endsWith("?")) {
+				System.out.println("It is question sentence.");
+				String first_word = data[0].toLowerCase();
+				String whWord = "how,what,who,which,when,why";
+				System.out.println("First of the sentence: "+data[0]);
+				if(whWord.contains(first_word)) {
+					System.out.println("It is wh-question.");
 				}
-			}
-			if(result == 1) {
-				System.out.println("It is past tense.");
+				int result = -1;
+				
+				for(int i = 0 ; i < ques.length ;i++) {
+					if(sentence.contains(ques[i])){
+						result = 1;
+						break;
+					}
+				}
+				if(result == 1) {
+					System.out.println("It is past tense.");
+				}else {
+					System.out.println("It is present tense.");
+
+				}
+
 			}else {
-				System.out.println("It is present tense.");
+				System.out.println("It is not question sentence.");
 
 			}
-
-		}else {
-			System.out.println("It is not question sentence.");
-
 		}
 	
-	}
+	
 
 }
